1bb4f4967c263baee875602de15926a4dd56904c
Kelvin <kmwendwa476@gmail.com>
2022-08-21 13:47:09 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
 
+  #setter method for name
+  def name=(name)
+    @name = name
+
+  end
+
+  #getter method
+  def name
+    @name
+  end
+
+  #setter method for breed
+  def breed=(breed)
+    @breed = breed
+  end
+
+  #getter method
+  def breed
+    @breed
+  end
+
 end
